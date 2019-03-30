6320b45e8ef9c4ea4476c0b39cdbab870e1c2533
Wyatt Haak <wyatt.haak4@gmail.com>
2019-03-30 16:19:47 -0700

    Added Screen

--- main.py
+++ main.py
@@ -4,8 +4,14 @@ import const
 
 
 def main():
+	pygame.display.set_mode((0,0), pygame.RESIZABLE)
+	running = True
 
 
+	while running:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				running = False
 
 
 
